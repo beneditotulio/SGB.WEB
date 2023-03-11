e086bbf0d3afe5ab7610ae90fc67f9a1723a6d02
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-03-11 07:57:18 +0200

    Adding Reserva Model

---
+++ SGB.WEB/Models/Reserva.cs
@@ -0,0 +1,11 @@
+﻿namespace SGB.WEB.Models
+{
+    public class Reserva
+    {
+        public Livro Livro { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime DataReserva { get; set; }
+        public DateTime DataExpiracao { get; set; }
+    }
+
+}
