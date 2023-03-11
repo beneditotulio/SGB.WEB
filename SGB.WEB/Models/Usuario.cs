d9596722981b0cce51ef30ec158b8738c2836c9c
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-03-11 07:58:46 +0200

    Adding User Model

---
+++ SGB.WEB/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace SGB.WEB.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public int NumeroCarteira { get; set; }
+        public List<Emprestimo> HistoricoEmprestimos { get; set; }
+        public List<Multa> MultasEmAberto { get; set; }
+    }
+
+}
