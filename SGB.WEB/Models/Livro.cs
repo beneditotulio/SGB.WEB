fc476a225d55ce98852a3b150043b43508c787fb
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-03-11 07:56:21 +0200

    Adding Emprestimo

---
+++ SGB.WEB/Models/Livro.cs
@@ -0,0 +1,14 @@
+﻿namespace SGB.WEB.Models
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+        public string ISBN { get; set; }
+        public int NumeroExemplaresDisponiveis { get; set; }
+        public List<Reserva> Reservas { get; set; }
+        public List<Emprestimo> Emprestimos { get; set; }
+    }
+
+}
