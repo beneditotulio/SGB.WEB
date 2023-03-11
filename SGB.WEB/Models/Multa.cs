6b74dcd60d4e428415e13a2c9ab4593b3054ffa6
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-03-11 08:00:48 +0200

    Adding multa

--- SGB.WEB/Models/Multa.cs
+++ SGB.WEB/Models/Multa.cs
@@ -2,5 +2,10 @@
 {
     public class Multa
     {
+        public Usuario Usuario { get; set; }
+        public decimal ValorMulta { get; set; }
+        public DateTime DataAplicacao { get; set; }
+        public DateTime? DataPagamento { get; set; }
     }
+
 }
