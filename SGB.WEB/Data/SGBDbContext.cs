f46990b1f432ec53e2e2578fd02ad85c6e9e79c2
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-03-12 08:07:16 +0200

    Adding SGB Database context

---
+++ SGB.WEB/Data/SGBDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGB.WEB.Models;
+
+namespace SGB.WEB.Data
+{
+    public class SGBDbContext : DbContext
+    {
+        DbSet<Usuario> Usuarios { get; set; }
+        DbSet<Emprestimo> Emprestimos { get; set; }  
+        DbSet<Livro> Livros { get; set; }   
+        DbSet<Multa> Multas { get; set; }
+        DbSet<Reserva>  Reservas { get; set; }
+    }
+}
